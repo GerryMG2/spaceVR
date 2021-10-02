2ed091671b886e8e7f76a81e0d5a1f7af7cd53ff
GerryMG2 <00032515@uca.edu.sv>
2021-10-02 13:29:32 -0600

    firts commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    public bool IsTriggerPressed()
+    {
+        return Google.XR.Cardboard.Api.IsTriggerPressed;
+        
+    }
+   
+}
